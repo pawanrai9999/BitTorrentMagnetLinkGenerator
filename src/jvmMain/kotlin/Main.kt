0398415b089b9997cbf7328fc6b5f2a61217a932
Pawan Rai <pawanrai9999@gmail.com>
2023-06-04 20:18:53 +0530

    Centered All Elements Vertically & Horizontally

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -13,10 +13,8 @@
  */
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -25,7 +23,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
@@ -40,6 +37,8 @@ fun App() {
 
     MaterialTheme {
         Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.padding(8.dp)
         ) {
             magnetHash()
@@ -127,12 +126,11 @@ fun magnetHash() {
 fun customTrackers() {
     var trackers by remember { mutableStateOf("") }
 
-    TextField(
+    OutlinedTextField(
         value = trackers,
         onValueChange = { trackers = it },
         label = { Text(text = "Custom Tracker(one per line)") },
         modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(10.dp)
-            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(8.dp))
     )
 }
 
