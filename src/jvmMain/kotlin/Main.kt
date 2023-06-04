0f90a5cd91cc009d4bbc82abe8ec337403f46e2d
Pawan Rai <pawanrai9999@gmail.com>
2023-06-04 16:47:10 +0530

    Added Dropdown for selecting tracker

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -13,13 +13,20 @@
  */
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
@@ -35,7 +42,62 @@ fun App() {
             magnetHash()
             Spacer(modifier = Modifier.size(4.dp))
             torrentName()
+            Spacer(modifier = Modifier.size(4.dp))
+            Row {
+                selectTrackers()
+                Button(
+                    onClick = {},
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                ) { Text(text = "Genereate Magnet Link") }
+            }
+        }
+    }
+}
 
+@Composable
+fun selectTrackers() {
+    val trackersList = arrayOf("STABLE", "UDP", "HTTP", "LIVE", "ALL")
+    var isDropdownExpanded by remember { mutableStateOf(false) }
+    var selectedTracker by remember { mutableStateOf(trackersList[0]) }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+
+    var icon = if (isDropdownExpanded) {
+        Icons.Filled.KeyboardArrowUp
+    } else {
+        Icons.Filled.KeyboardArrowDown
+    }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        OutlinedTextField(
+            value = selectedTracker,
+            onValueChange = { selectedTracker = it },
+            modifier = Modifier.onGloballyPositioned { coordinates ->
+                textFieldSize = coordinates.size.toSize()
+            },
+            label = { Text(text = "Choose Tracker Group") },
+            trailingIcon = {
+                Icon(icon, "dropdown", Modifier.clickable { isDropdownExpanded = !isDropdownExpanded })
+            },
+            readOnly = true
+        )
+
+        DropdownMenu(
+            expanded = isDropdownExpanded,
+            onDismissRequest = { isDropdownExpanded = false },
+            modifier = Modifier.width(
+                with(LocalDensity.current) { textFieldSize.width.toDp() }
+            )
+        ) {
+            trackersList.forEach { label ->
+                DropdownMenuItem(
+                    onClick = {
+                        selectedTracker = label
+                        isDropdownExpanded = false
+                    }
+                ) {
+                    Text(text = label)
+                }
+            }
         }
     }
 }
